d04414a83813e9f937f57daef3990f6f0b10bb94
Bertrand D <b.duclot@gmail.com>
2023-12-05 13:08:14 +0100

    Ex3

--- Exercice 2.js
+++ Exercice 2.js
@@ -8,6 +8,7 @@ console.log(couleur2);*/
 // Link https://www.codewars.com/kata/559d2284b5bb6799e9000047/train/javascript
 // String array and mixed array
 
+// With map function
 function addLength(str) {
   // Divisez la chaîne en mots
   const words = str.split(" ");
@@ -15,6 +16,24 @@ function addLength(str) {
   // Utilisez la méthode map pour créer un nouveau tableau
   // où chaque élément est une chaîne contenant le mot et sa longueur
   const resultArray = words.map((word) => `${word} ${word.length}`);
+
+  return resultArray;
+}
+
+// With for loop
+
+function addLength(str) {
+  // Divisez la chaîne en mots
+  const words = str.split(" ");
+
+  // Créez un nouveau tableau pour stocker les résultats
+  const resultArray = [];
+
+  // Utilisez une boucle for pour itérer sur chaque mot
+  for (let i = 0; i < words.length; i++) {
+    // Ajoutez chaque mot et sa longueur au tableau de résultats
+    resultArray.push(`${words[i]} ${words[i].length}`);
+  }
 
   return resultArray;
 }
---
+++ Exercice 3.js
@@ -0,0 +1,58 @@
+// Who is going to pay for the wall?, link https://www.codewars.com/kata/58bf9bd943fadb2a980000a7
+
+/*function whoIsPaying(name) {
+  // Pour chaque mot parcourir la longueur du mot et ne retenir que ça première lettre
+
+  let truncatedName = [];
+  const separatedWords = name.split(" ");
+  console.log("full name : ", name);
+
+  if (name.length <= 2) {
+    console.log("premiere cond");
+    truncatedName = name;
+    return truncatedName;
+  } else {
+    console.log("deuxième cond");
+    for (let j = 0; j < 1; j++) {
+      for (let i = 0; i < separatedWords.length; i++) {
+        truncatedName[i] = separatedWords[i][j];
+      }
+    }
+  }
+
+  return truncatedName;
+}
+
+console.log(whoIsPaying("test exercice"));*/
+
+/* Other exercice */
+
+/*function whoIsPaying(name) {
+  // Pour chaque mot parcourir la longueur du mot et ne retenir que ça première lettre
+
+  let result = [];
+  //result.push(name);
+
+  if (name === "") {
+    result.push("");
+  } else if (name.length <= 2) {
+    result.push(name[0] + [name[1]]);
+  } else {
+    result.push(name);
+
+    for (let i = 1; i <= 1; i++) {
+      result[i] = name[0] + name[1];
+    }
+  }
+  return result;
+}
+
+console.log(whoIsPaying("Mexico"));
+
+/* Shorter solution */
+
+function whoIsPaying(name) {
+  return name.length > 2 ? [name, name.substr(0, 2)] : [name];
+}
+
+console.log(whoIsPaying(""));
