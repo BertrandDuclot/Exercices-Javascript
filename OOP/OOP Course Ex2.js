7c9d00f1121b6d8fbe5bb7737fb4ed32ed86ed0a
Bertrand D <b.duclot@gmail.com>
2023-12-06 12:42:19 +0100

    OOP Ex2

--- OOP/OOP Course Ex2.js
+++ OOP/OOP Course Ex2.js
@@ -1,5 +1,4 @@
 // ES6 Classes
-
 class Person {
   constructor(firstName, birthYear) {
     this.firstName = firstName;
@@ -13,14 +12,42 @@ class Person {
   greet() {
     console.log("hello " + this.firstName);
   }
+
+  get age() {
+    return console.log("Age test");
+  }
 }
 
 const roger = new Person("roger", 1950);
 console.log(roger);
 
-// Calling method
+// Calling methods
 roger.calcAge();
 
 console.log(roger.__proto__ == Person.prototype);
 
 roger.greet();
+
+roger.age;
+
+// Hoisted
+
+// Getters and Setters
+
+const account = {
+  owner: "roger",
+  movements: [200, 350, 620],
+
+  get latest() {
+    return this.movements.slice(-1).pop();
+  },
+
+  set latest(mov) {
+    this.movements.push(mov);
+  },
+};
+
+console.log(account.latest);
+
+account.test = 50;
+console.log(account.movements);
