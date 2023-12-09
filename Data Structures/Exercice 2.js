f86b26148209e85c3816677725e61204329f196f
Bertrand D <b.duclot@gmail.com>
2023-12-09 17:51:38 +0100

    Rest parameter

--- Data Structures/Exercice 2.js
+++ Data Structures/Exercice 2.js
@@ -5,12 +5,37 @@
 const arr = [7, 8, 9];
 
 const newArr = [1, 2, ...arr, "test"];
-console.log(newArr);
 
 //const mainMenuCopy = [...restaurant.mainMenu];
 
 // Iterables ; arrays, strings, maps, sets, not objects
 const str = "test";
 const letters = [...str, "", "END"];
-console.log(letters);
-console.log(...str);
+
+/////////////////////////////////////////////////////////
+///////////////////// Rest Pattern //////////////////////
+/////////////////////////////////////////////////////////
+
+const array = [1, 2, ...[3, 4]];
+const [a, b, ...others] = [1, 2, 3, 4, 5];
+
+/*
+const [pizza, , risotto, ...otherFord] = [
+  ...restaurant.mainMenu,
+  ...restaurant.starterMenu,
+];*/
+
+// Objects
+/*const { sat, ...weekdays } = restaurant.openingHours;*/
+
+// 2. Functions
+const add = function (...numbers) {
+  let sum = 0;
+  for (let i = 0; i < numbers.length; i++) sum += numbers[i];
+  console.log(sum);
+};
+
+add(2, 3, 7);
+
+const x = [10, 3, 2];
+add(...x);
