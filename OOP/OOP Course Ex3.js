fa710fc1403fc675546f35270abf4327b689b292
Bertrand D <b.duclot@gmail.com>
2023-12-09 10:16:03 +0100

    OOP Ex3

--- OOP/OOP Course Ex3.js
+++ OOP/OOP Course Ex3.js
@@ -38,7 +38,7 @@ Student.prototype.constructor = Student;
 // Inheritance
 ///////////////////////////////////////////////////////
 
-class Student extends Person {
+/*class Student extends Person {
   constructor(firstName, birthYear, course) {
     super(firstName, birthYear);
     this.course = course;
@@ -46,4 +46,55 @@ class Student extends Person {
 }
 
 const martha = new Student("Martha", 2012, "CS");
-console.log(martha);
+console.log(martha);*/
+
+///////////////////////////////////////////////////////
+// Chaining example
+///////////////////////////////////////////////////////
+
+/*withdraw(val) {
+  this.deposit(-val)
+  return this;
+}*/
+
+// Result
+
+class Student extends Person {
+  university = "univ test";
+  #studyhours = 0;
+  #course;
+  static numSubjects = 10;
+
+  constructor(fullName, birthYear, startYear, course) {
+    super(fullName, birthYear);
+    this.startYear = startYear;
+    this.#course = course;
+  }
+
+  introduce() {
+    console.log(`I study ${this.#course} at ${this.university}`);
+  }
+
+  study(h) {
+    this.#makeCoffe();
+    this.#studyhours += h;
+  }
+
+  #makeCoffe() {
+    return "Here is a coffe !";
+  }
+
+  get testScore() {
+    return this._testScore;
+  }
+
+  set testScore(score) {
+    this._testScore = score <= 20 ? score : 0;
+  }
+
+  static printCurriculum() {
+    console.log(`There are ${this.numSubjects} subjects`);
+  }
+}
+
+const student = new Student("roger", 1980, 2000, "Computer Science");
