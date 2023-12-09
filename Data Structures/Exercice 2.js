bf68c67588e75491f3a334d205215b7bfa1983bb
Bertrand D <b.duclot@gmail.com>
2023-12-09 16:47:15 +0100

    Data structure Ex2

--- Data Structures/Exercice 2.js
+++ Data Structures/Exercice 2.js
@@ -1,1 +1,16 @@
-console.log("te'st");
+// Art attack
+
+// Spread operator ...
+
+const arr = [7, 8, 9];
+
+const newArr = [1, 2, ...arr, "test"];
+console.log(newArr);
+
+//const mainMenuCopy = [...restaurant.mainMenu];
+
+// Iterables ; arrays, strings, maps, sets, not objects
+const str = "test";
+const letters = [...str, "", "END"];
+console.log(letters);
+console.log(...str);
